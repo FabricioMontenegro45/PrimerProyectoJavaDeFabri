3cd60d6d25b3662e31a42370aea5a48cf92274e3
FabricioMontenegro45 <129108739+FabricioMontenegro45@users.noreply.github.com>
2023-04-13 16:26:18 -0300

    Mi primer Commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+      boolean varBoolean = true;
+        System.out.println("varBoolean = " + varBoolean);
+
+        if(varBoolean) {
+            System.out.println("la bandera es verdadera");
+
+        }
+        else {
+            System.out.println("la bandera es falsa");
+        }
+        var edad = 10;
+        //var esAdulto = edad >= 18;
+        if(edad >= 18) {
+            System.out.println("eres mayor de edad");
+        }
+        else {
+            System.out.println("eres menor de edad");
+        }
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
